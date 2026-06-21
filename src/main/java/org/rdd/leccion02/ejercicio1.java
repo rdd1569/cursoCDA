535c9135b61a776c0d3949d13f3dcc00fde1038e
rdd1569 <rdd1569@gmail.com>
2026-06-21 14:03:11 -0300

    primer commit

---
+++ src/main/java/org/rdd/leccion02/ejercicio1.java
@@ -0,0 +1,12 @@
+package org.rdd.leccion02;
+
+public class ejercicio1 {
+
+    public static void main(String[] args){
+        String[] listaNombres=("Juan")
+        char[] nombres = listaNombres;[]
+        imprimirElementos(listaNombres);
+        return();
+
+    }
+}
