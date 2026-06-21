3c64293b18622d1cd2739e2f60adb08d6e47a97a
rdd1569 <rdd1569@gmail.com>
2026-06-21 18:23:36 -0300

    clase imprimirElementos

--- src/main/java/org/rdd/leccion02/ejercicio1.java
+++ src/main/java/org/rdd/leccion02/ejercicio1.java
@@ -3,10 +3,14 @@ package org.rdd.leccion02;
 public class ejercicio1 {
 
     public static void main(String[] args){
-        String[] listaNombres=("Juan")
-        char[] nombres = listaNombres;[]
+        String[] listaNombres = {"Juan", "Raul", "Dora"};
         imprimirElementos(listaNombres);
-        return();
-
+    }
+    private static void imprimirElementos(String[] lista) {
+       // Sintaxis: (inicio; condición; incremento)
+        for (int i = 0; i < lista.length; i++) {
+            int j = i + 1;
+            System.out.println("Nombre " + j + ": " + lista[i]);
+        }
     }
 }
