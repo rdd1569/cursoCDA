5696e356f17b7b6e2e9e799f4020459188a50b00
rdd1569 <rdd1569@gmail.com>
2026-06-21 22:50:44 -0300

    todo menos map

--- src/main/java/org/rdd/leccion02/Ejercicio1.java
+++ src/main/java/org/rdd/leccion02/Ejercicio1.java
@@ -12,18 +12,31 @@ public class Ejercicio1 {
         } else {
             System.out.println("No está " + clave);
         }
+
+        String sub = "u";
+        ImprimirElementos(HitSubstring(listaNombres,sub));
     }
     private static void ImprimirElementos(String[] lista) {
        // Sintaxis: (inicio; condición; incremento)
         for (int i = 0; i < lista.length; i++) {
             int j = i + 1;
-            System.out.println("Nombre " + j + ": " + lista[i]);
+            if (lista[i] != null) {
+                System.out.println("Nombre " + j + ": " + lista[i]);
+            }
         }
     }
     private static boolean HitElemento(String[] lista, String elemento) {
         Boolean salida = false;
         for (int i = 0; i < lista.length; i++) {
             if (lista[i].equals(elemento)) salida = true;
+        }
+        return (salida);
+    }
+    private static String[] HitSubstring(String[] lista, String substring){
+        int k = 0;
+        String salida[]= new String[lista.length];
+        for (int i = 0; i < lista.length; i++) {
+             if (lista[i].contains(substring)) {salida[k] = lista[i]; k++;}
         }
         return (salida);
     }
