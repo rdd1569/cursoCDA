eeb0f88ef0d5028fd247a89d63ed3bea2319ef94
rdd1569 <rdd1569@gmail.com>
2026-06-21 21:19:01 -0300

    método hitElemento

--- src/main/java/org/rdd/leccion02/ejercicio1.java
+++ src/main/java/org/rdd/leccion02/ejercicio1.java
@@ -1,10 +1,19 @@
 package org.rdd.leccion02;
 
+import java.util.Arrays;
+
 public class ejercicio1 {
 
     public static void main(String[] args){
         String[] listaNombres = {"Juan", "Raul", "Dora"};
         imprimirElementos(listaNombres);
+
+        String clave = "Raul";
+        if (hitElemento(listaNombres, clave)){
+            System.out.println("Encontrado " + clave);
+        } else {
+            System.out.println("No está " + clave);
+        }
     }
     private static void imprimirElementos(String[] lista) {
        // Sintaxis: (inicio; condición; incremento)
@@ -12,5 +21,12 @@ public class ejercicio1 {
             int j = i + 1;
             System.out.println("Nombre " + j + ": " + lista[i]);
         }
+    }
+    private static boolean hitElemento(String[] lista, String elemento) {
+        Boolean salida = false;
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i].equals(elemento)) salida = true;
+        }
+        return (salida);
     }
 }
