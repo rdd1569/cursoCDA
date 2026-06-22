285627834acfc6d626c12413cb43d166e9a6d7b4
rdd1569 <rdd1569@gmail.com>
2026-06-21 21:33:18 -0300

    revisado

--- src/main/java/org/rdd/leccion02/Ejercicio1.java
+++ src/main/java/org/rdd/leccion02/Ejercicio1.java
@@ -1,28 +1,26 @@
 package org.rdd.leccion02;
 
-import java.util.Arrays;
-
-public class ejercicio1 {
+public class Ejercicio1 {
 
     public static void main(String[] args){
         String[] listaNombres = {"Juan", "Raul", "Dora"};
-        imprimirElementos(listaNombres);
+        ImprimirElementos(listaNombres);
 
         String clave = "Raul";
-        if (hitElemento(listaNombres, clave)){
+        if (HitElemento(listaNombres, clave)){
             System.out.println("Encontrado " + clave);
         } else {
             System.out.println("No está " + clave);
         }
     }
-    private static void imprimirElementos(String[] lista) {
+    private static void ImprimirElementos(String[] lista) {
        // Sintaxis: (inicio; condición; incremento)
         for (int i = 0; i < lista.length; i++) {
             int j = i + 1;
             System.out.println("Nombre " + j + ": " + lista[i]);
         }
     }
-    private static boolean hitElemento(String[] lista, String elemento) {
+    private static boolean HitElemento(String[] lista, String elemento) {
         Boolean salida = false;
         for (int i = 0; i < lista.length; i++) {
             if (lista[i].equals(elemento)) salida = true;
